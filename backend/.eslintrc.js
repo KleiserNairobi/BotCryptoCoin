d8be8d5ce712016f9117bf8639d0c5a50c6b69de
Kleiser Nairobi <kleiser.nairobi@gmail.com>
2024-05-05 12:14:03 -0300

    Iniciando backend com NestJS

---
+++ backend/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'no-shadow': 'off',
+    'no-undef': 'off',
+    'no-console': 'error',
+    'prettier/prettier': 'off',
+    'jsx-quotes': ['error', 'prefer-single'],
+    '@typescript-eslint/no-shadow': ['off'],
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
